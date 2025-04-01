54144ecc5b254ce68eaf89348682fa39e3fe31ac
rcAsironman <karthikmangineni0603@gmail.com>
2025-04-01 11:42:51 -0500

    fix: active tab switching logic in navigation bar

--- app/components/navigationBar.tsx
+++ app/components/navigationBar.tsx
@@ -1,21 +1,33 @@
-const BottomNav = () => {
-    return (
-      <nav className="absolute bottom-0 w-full bg-white border-t flex justify-around py-2 shadow-md">
-        <button className="flex flex-col items-center text-gray-700">
-          <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
-          <span className="text-xs">Home</span>
-        </button>
-        <button className="flex flex-col items-center text-gray-700">
-          <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
-          <span className="text-xs">Search</span>
-        </button>
-        <button className="flex flex-col items-center text-gray-700" >
-          <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
-          <span className="text-xs">Profile</span>
-        </button>
-      </nav>
-    );
-  };
-  
-  export default BottomNav;
-  
+'use client'
+import { useRouter } from "next/navigation";
+import { IoSearch } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+
+const BottomNav = ({ activeTab, setActiveTab }: { activeTab: string; setActiveTab: (tab: string) => void }) => {
+
+  const router = useRouter();
+
+  return (
+    <nav className="absolute bottom-0 w-full bg-white border-t flex justify-around py-2 shadow-md">
+      <button className="flex flex-col items-center text-gray-700"
+        onClick={() => setActiveTab('search')}
+      >
+        <div className={`rounded-full p-1 ${activeTab === 'search' ? 'bg-black' : ''}`}>
+          <IoSearch size={25} color={activeTab === 'search' ? 'white' : 'gray'}/>
+        </div>
+        <span className={`text-xs ${activeTab === 'search'? 'text-black-500' : 'text-gray-300'}`}>Search</span>
+      </button>
+      <button className="flex flex-col items-center text-gray-700"
+        onClick={() => setActiveTab('profile')}
+      >
+        <div className={`rounded-full p-1 ${activeTab === 'profile'? 'text-black-500 bg-black' : 'text-gray-300'} 
+        `}>
+          <CgProfile size={25} color={activeTab === 'profile' ? 'white' : 'gray'}/>
+        </div>
+        <span className={`text-xs ${activeTab === 'profile'? 'text-black-500' : 'text-gray-300'}`}>Profile</span>
+      </button>
+    </nav>
+  );
+};
+
+export default BottomNav;
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,18 @@
-import Image from "next/image";
-import CameraButton from "@/app/components/cameraButton";
+'use client'
+import { useState } from "react";
 import NavigationBar from "@/app/components/navigationBar";
+import Search from "@/app/components/search";
+import Profile from "@/app/components/profile";
 
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState('search');
+  console.log(activeTab);
   return (
-    <div className="flex items-center justify-center h-screen relative">
-      <div className="flex flex-col items-center">
-        <main className="w-80 h-80 border-2 border-gray-300 rounded-lg overflow-hidden shadow-lg flex items-center justify-center bg-black-100">
-          <Image
-            className="w-full h-full object-cover"
-            src="/sample.jpeg"
-            alt="Space for Image Display"
-            width={300}
-            height={300}
-          />
-        </main>
-
-        <div className="mt-4 flex space-x-4">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md">
-            Select Image
-          </button>
-          <CameraButton />
-        </div>
-      </div>
-      <NavigationBar/>
+    <div className="flex items-center justify-center h-screen relative bg-white">
+      {activeTab === 'search' && <Search/>}
+      {activeTab === 'profile' && <Profile/>}
+      <NavigationBar activeTab={activeTab} setActiveTab={setActiveTab}/>
     </div>
   );
 }
